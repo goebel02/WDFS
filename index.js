45e2bd8abeff93ef8212436dfe2915efb95b0a41
Tyler Goebel <tyler.goebel@wwt.com>
2020-01-31 16:18:48 -0600

    updated port to be dynamic

--- index.js
+++ index.js
@@ -9,9 +9,9 @@ const HapiSwagger = require('hapi-swagger');
 
 
 const init = async () => {
-
+  
     const server = Hapi.server({
-        port: 3000,
+        port: process.env.PORT || 3000,
         host: 'localhost'
     });
 
